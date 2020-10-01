5e7c4b6e8f12a2b9e807d12ca957675395881148
visa4 <antonino.visalli@gmail.com>
2020-10-01 17:52:50 +0200

    Fix user

--- src/Module/User/Event/UserPasswordEvent.php
+++ src/Module/User/Event/UserPasswordEvent.php
@@ -30,11 +30,10 @@ class UserPasswordEvent {
      */
     public function __invoke(EventInterface $event) {
 
+        var_dump($event->getTarget()->getPassword());
+        die();
         $event->getTarget()->setPassword($this->crypto->crypto(
             $event->getTarget()->getPassword()
         ));
-
-        var_dump( $event->getTarget());
-        die();
     }
 }
