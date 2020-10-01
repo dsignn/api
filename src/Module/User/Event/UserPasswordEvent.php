70b0d3914f5da5e7ed68e5a53e57a330b730c199
visa4 <antonino.visalli@gmail.com>
2020-10-01 17:17:15 +0200

    Test user

--- src/Module/User/Event/UserPasswordEvent.php
+++ src/Module/User/Event/UserPasswordEvent.php
@@ -29,10 +29,12 @@ class UserPasswordEvent {
      * @param EventInterface $event
      */
     public function __invoke(EventInterface $event) {
-var_dump('fdsfds');
-die();
+
         $event->getTarget()->setPassword($this->crypto->crypto(
             $event->getTarget()->getPassword()
         ));
+
+        var_dump('ggggggggggggg');
+        die();
     }
 }
