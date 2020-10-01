c9151b4823de89f8c583565e7f98ac8d8b37bbcc
visa4 <antonino.visalli@gmail.com>
2020-10-01 17:34:13 +0200

    Fix user

--- src/Module/Oauth/repositories.php
+++ src/Module/Oauth/repositories.php
@@ -44,10 +44,11 @@ return function (ContainerBuilder $containerBuilder) {
         'OAuthCrypto' => function(ContainerInterface $c) {
 
             $key = null;
+            $content = trim(file_get_contents(__DIR__ . '/../../../key/dsign-oauth-password.txt'));
             if (file_exists(__DIR__ . '/../../../key/dsign-oauth-password.txt')) {
-                $key = Key::loadFromAsciiSafeString(file_get_contents(__DIR__ . '/../../../key/dsign-oauth-password.txt'));
+                $key = Key::loadFromAsciiSafeString($content);
             }
-            var_dump(file_get_contents(__DIR__ . '/../../../key/dsign-oauth-password.txt'));
+            var_dump($content);
             var_dump($key);
             die();
             return new DefuseCrypto($key);
