3b25088a2f470eeca26260dd320b75f4990f9268
visa4 <antonino.visalli@gmail.com>
2021-02-16 09:08:57 +0100

    Test

--- src/Module/Oauth/Repository/UserRepository.php
+++ src/Module/Oauth/Repository/UserRepository.php
@@ -45,6 +45,8 @@ class UserRepository implements UserRepositoryInterface {
     public function getUserEntityByUserCredentials($username, $password, $grantType, ClientEntityInterface $clientEntity) {
 
         $resultSet = $this->storage->getAll(['email' => $username]);
+        var_dump('test');
+        die();
         $user = null;
         switch (true) {
             case $resultSet->count() === 1:
