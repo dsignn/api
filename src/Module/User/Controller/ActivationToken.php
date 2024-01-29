75190718e2d91f48e77afad8340a3ceec4b8639d
visa4 <antonino.visalli@gmail.com>
2024-01-29 15:00:26 +0100

    Fix activation token

--- src/Module/User/Controller/ActivationToken.php
+++ src/Module/User/Controller/ActivationToken.php
@@ -51,12 +51,10 @@ class ActivationToken implements RpcControllerInterface {
             return $this->getAcceptData($request, $response, ['errors' => 'No token in query string']);
         }
 
-        var_dump($data);
-        die();
         $resultSet = $this->storage->getAll(['activation_code.token' => $data['token']]);
         /** @var UserEntity $user */
         $user = $resultSet->current();
-        if (!$user) {
+        if (!$user) {c
             return $response->withStatus(404);
         }
 
